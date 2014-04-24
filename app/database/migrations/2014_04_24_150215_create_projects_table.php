8fd3845030c85ba938e5eb808decab25114bb298
Francesc Pellicero <pelly.obn91@gmail.com>
2014-04-24 08:22:41 -0700

    Added projects table migration

---
+++ app/database/migrations/2014_04_24_150215_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function ($t)
+		{
+			$t->increments('id');
+			$t->string('title', 64);
+			$t->longtext('description');
+			$t->string('ciutat', 32);
+			$t->integer('author_id');
+
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
