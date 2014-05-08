3f1bd66b8d59cb48ca618d54bcf944144db52c20
Francesc Pellicero <pelly.obn91@gmail.com>
2014-05-08 07:44:15 -0700

    User info box

--- app/views/user/view.blade.php
+++ app/views/user/view.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class='well profile_info row'>
-	<div class='col-md-2 col-md-offset-1'>
-		{{ HTML::image('img/profile_blank.jpg') }}
-	</div>
-	<div id='user_info' class='col-md-6 col-md-offset-1'>
-		<h3>{{$user->name}} {{$user->surname}}</h3>
-		<h4>ESTUDIANT a CIUTAT</h4>
-		<h4 id='occupation_info'>UNIVERSITAT ACTUAL i CURS</h4>
-		<div id='logged_in_controls' class='well'>
-			<a href="{{URL::action('newProject')}}">
-				<button type="button" class="btn btn-primary">
-					<span class='glyphicon glyphicon-plus-sign'></span>
-					Crear nou projecte
-				</button>
-			</a>
-			<a href="{{URL::action('editUserProfile', array($user->id)) }}">
-				<button type='button' class='btn btn-success'>
-					<span class='glyphicon glyphicon-pencil'></span>
-					Modificar Perfil
-				</button>
-			</a>
+<div class='row'>
+		<div id='profile_img' class='col-md-2 col-md-offset-2'>
+			{{ HTML::image('img/profile_blank.jpg') }}
 		</div>
-	</div>
+		<div id='user_info' class='col-md-6'>
+			<h3>{{$user->name}} {{$user->surname}}</h3>
+			<h3><small>CIUTAT</small></h3>
+			<h4><small>UNIVERSITAT ACTUAL i CURS</small></h4>
+			<div id='logged_in_controls' class='well'>
+				<a href="{{URL::action('newProject')}}">
+					<button type="button" class="btn btn-primary">
+						<span class='glyphicon glyphicon-plus-sign'></span>
+						Crear nou projecte
+					</button>
+				</a>
+				<a href="{{URL::action('editUserProfile', array($user->id)) }}">
+					<button type='button' class='btn btn-success'>
+						<span class='glyphicon glyphicon-pencil'></span>
+						Modificar Perfil
+					</button>
+				</a>
+			</div>
+		</div>
 </div>
+<br>
 
 <div class='row'>
 	@foreach($projects as $index => $project)
