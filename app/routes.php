da6073632102b62feb749e61ec0e6bbb28ff035c
Francesc Pellicero <pelly.obn91@gmail.com>
2014-05-12 11:13:34 -0700

    Updated Laravel and User Model

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', array('as' => 'home', 'uses' => 'HomeController@homePage'));
+Route::get('/', array('as' => 'home', 'uses' => 'HomeController@homePage'))->before('auth');;
 
 /*
  * Routes handling the login system.
