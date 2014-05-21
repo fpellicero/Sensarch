51fd8aa3cff006b20a521a7da0982bb0364bda9d
Francesc Pellicero <pelly.obn91@gmail.com>
2014-05-21 10:03:34 -0700

    New profile layout

--- app/views/blocks/project.blade.php
+++ app/views/blocks/project.blade.php
@@ -1,7 +1,7 @@
 <div class='project_old'>
 	<div class='project_cover_old'>
 		<a href="{{ URL::route('showProject', $project->id) }}">
-			<img class='project_cover_old' src="http://upload.wikimedia.org/wikipedia/commons/6/6f/Disney_Concert_Hall_by_Carol_Highsmith.jpg">
+			<img class='project_cover_old' src="{{ $project->getCoverImgURL() }}">
 		</a>
 	</div>
 	<div class='project_info_old'>
