01b1ee5ab3975ba7390336831a99a374eab29b3f
Francesc Pellicero <pelly.obn91@gmail.com>
2014-05-29 11:02:44 -0700

    Nou bloc de projecte

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,18 +4,20 @@
 
 <div class='container'>
 	<div class='col-md-3'>
-		<span id='beta'>BETA</span>
-		<p style='text-align: left;'>
-			<i>
-				Si tienes dudas, comentarios, sugerencias... háznoslo saber en
-				<a href="mailto:info@sensarch.com">info@sensarch.com</a>
-			</i>
-		</p>
+		<div id='beta_home'>
+			<span id='beta'>BETA</span>
+			<p style='text-align: left;'>
+				<i>
+					Si tienes dudas, comentarios, sugerencias... háznoslo saber en
+					<a href="mailto:info@sensarch.com">info@sensarch.com</a>
+				</i>
+			</p>
+		</div>
 	</div>
 
 	<div class='col-md-9'>
 		@foreach($projects as $index => $project)
-		@include('blocks.project', array('project' => $project, 'user' => User::find($project->author_id)))
+			@include('blocks.project', array('project' => $project, 'user' => User::find($project->author_id)))
 		@endforeach
 	</div>
 </div>
--- app/views/layouts/sections/footer.blade.php
+++ app/views/layouts/sections/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="footer" class='row' style="background: #FFFFFF;">
 	<ul class="icons">
-		<li><a href="http://www.facebook.com/sensarch/" target="_blank" class="fa fa-twitter solo"><span>Twitter</span></a></li>
-		<li><a href="http://www.twitter.com/sensarchin/" target="_blank" class="fa fa-facebook solo"><span>Facebook</span></a></li>
+		<li><a href="https://twitter.com/sens_arch" target="_blank" class="fa fa-twitter solo"><span>Twitter</span></a></li>
+		<li><a href="https://www.facebook.com/sensarch" target="_blank" class="fa fa-facebook solo"><span>Facebook</span></a></li>
 		<li><a href="http://plus.google.com/103791116641554183460/" target="_blank" class="fa fa-google-plus solo"><span>Google+</span></a></li>
 	</ul>
 	<div class="copyright">
