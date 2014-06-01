a8823f80ed4aa58e8ac151da14e55d08c52560ab
Francesc Pellicero <pelly.obn91@gmail.com>
2014-06-01 08:26:37 -0700

    Informació bàsica usuari

--- app/views/user/view.blade.php
+++ app/views/user/view.blade.php
@@ -5,21 +5,27 @@
 @stop
 
 @section('content')
+
 <div class='container' style='margin-top: -24px;'>
 	<div class='row'>
 		<div class='col-md-12'>
 			<div id='user_info_wrapper'>
 				<div id='profile_img'>
-					<img src="/img/profile_blank.png">
+					{{ HTML::image($user->getProfilePicURL()) }}
 				</div>
 				<div id='user_info'>
-					<h1>Nombre y Apellidos</h1>
-					<span>Sant Cugat del Vallès, Catalunya</span>
+					<h1>{{ $user->first_name }} {{ $user->last_name}}</h1>
+					@if ($user->current_address)
+						<span> {{ $user->current_address }}</span>
+					@endif
 
 					<div class='user_occupation'>
-						<span class='tag'>ACTUAL:</span> Colaborador en Sensarch
-						<br>
-						<span class='tag'>ANTERIOR:</span> Estudiante en Escola d'Arquitectura del Vallès
+						@if ($user->current_job)
+							<span class='tag'>ACTUAL:</span> {{ $user->current_job }} <br>
+						@endif
+						@if ($user->past_job)
+							<span class='tag'>ANTERIOR:</span> {{ $user->past_job }}
+						@endif
 					</div>
 
 					<div class='user_lang'>
@@ -27,30 +33,37 @@
 					</div>
 
 					<div class='user_social'>
-						<span>
-							<a href="#">
-								<i class="fa fa-twitter-square"></i>
-							</a>
-						</span>
+						@if ($user->twitter)
+							<span>
+								<a target='_blank' href="{{ $user->twitter }}">
+									<i class="fa fa-twitter-square"></i>
+								</a>
+							</span>
+						@endif
 
-						<span>
-							<a href="#">
-								<i class="fa fa-linkedin-square"></i>
-							</a>
-						</span>
+						@if ($user->linkedin)
+							<span>
+								<a target='_blank' href="{{ $user->linkedin }}">
+									<i class="fa fa-linkedin-square"></i>
+								</a>
+							</span>
+						@endif
 
-						<span>
-							<a href="#">
-								<i class="fa fa-facebook-square"></i>
-							</a>
-						</span>
+						@if ($user->facebook)
+							<span>
+								<a target='_blank' href="{{ $user->facebook }}">
+									<i class="fa fa-facebook-square"></i>
+								</a>
+							</span>
+						@endif
 
-						<span>
-							<a href="#">
-								<i class="fa fa-instagram"></i>
-							</a>
-						</span>
-
+						@if ($user->instagram)
+							<span>
+								<a target='_blank' href="{{ $user->instagram }}">
+									<i class="fa fa-instagram"></i>
+								</a>
+							</span>
+						@endif
 
 					</div>
 				</div>
@@ -60,15 +73,10 @@
 
 	<div class='row'>
 		<div class='col-md-2 hidden-sm hidden-xs'>
-			<div id='profile_sidebar'>
-				@include('blocks.user_info_new')->with('user', $user)
-			</div>
+			
 		</div>
 
 		<div class='col-md-9 col-md-offset-1 col-sm-12 col-xs-12'>
-			<div id='profile_top' class='hidden-md hidden-lg'>
-				@include('blocks.user_info')->with('user', $user)
-			</div>
 			<a href="{{ URL::route('newProject') }}">
 				@if(Sentry::check() && Sentry::getUser()->id == $user->id)
 				<div id='new_project_block'>
@@ -78,9 +86,11 @@
 				</div>
 				@endif
 			</a>
+
 			@foreach($projects as $index => $project)
-			@include('blocks.project', array('project' => $project, 'user' => NULL))
+				@include('blocks.project', array('project' => $project, 'user' => NULL))
 			@endforeach
+
 		</div>
 	</div>
 </div>
